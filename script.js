3e3f33ba4d3f14b8508acb339d46ca1cd4b7f0d9
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-25 18:53:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
 
 
+/*
+
 console.log("1");
 
 setTimeout(() => {
     console.log("2")
-},0);
+}, 0);
 
 Promise.resolve(3).then((res) => {
         console.log(res);
 });
 
 console.log("4");
+
+for( let i = 0; i < 1000000000; i++){
+
+}
+
+*/
+
+const prom = new Promise((resolve, reject) => {
+        if( new Date() < new Date("01-01-2024")){
+            reject("Error");
+        }
+        resolve("Success");
+});
+
+prom.then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.log(error);
+})
+
+
